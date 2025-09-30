3b420d3a09f923e141dd1754d40fb2a90521841f
baklava tiramisu <baklavatiramisu@proton.me>
2025-09-30 19:20:47 +0530

    Implement REST controller for managing users

---
+++ src/main/java/com/baklavatiramisu/learn/springjpa/controller/user/CreateUserRequest.java
@@ -0,0 +1,13 @@
+package com.baklavatiramisu.learn.springjpa.controller.user;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateUserRequest(@NotBlank String name, @NotBlank String handle) {
+    @Override
+    public String toString() {
+        return "CreateUserRequest{" +
+                "name='" + name + '\'' +
+                ", handle='" + handle + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/baklavatiramisu/learn/springjpa/repository/PostsRepository.java
@@ -0,0 +1,9 @@
+package com.baklavatiramisu.learn.springjpa.repository;
+
+import com.baklavatiramisu.learn.springjpa.dto.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostsRepository extends JpaRepository<Post, Long> {
+}
